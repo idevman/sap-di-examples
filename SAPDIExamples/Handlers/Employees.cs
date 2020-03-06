3dd99360d8a5f3132bf60435b6b358946f317b9c
Jesús Ildefonso Mirón <isc.sirius@gmail.com>
2020-03-06 01:36:46 -0600

    Agrega pruebas unitarias de empleados

---
+++ SAPDIExamples/Handlers/Employees.cs
@@ -0,0 +1,90 @@
+﻿using SAPbobsCOM;
+using SAPDIExamples.Extensions;
+using System.Collections.Generic;
+
+namespace SAPDIExamples.Handlers
+{
+
+    /// <summary>
+    /// Employees handler
+    /// </summary>
+    public class Employees
+    {
+
+        /// <summary>
+        /// Load first 2 employees
+        /// </summary>
+        /// <param name="db">Database connection</param>
+        /// <param name="prefix">Prefix first name</param>
+        /// <returns>Employee list data</returns>
+        public List<Dictionary<string, string>> LoadByPrefix(DBConnection db, string prefix)
+        {
+            db.SQLCommand.CommandText =
+                @"SELECT [empID], [firstName], [lastName], [Active]
+                  FROM [OHEM]
+                  WHERE [firstName] like @prefix
+                  ORDER BY [empID]";
+            db.SQLCommand.Parameters.Add("@prefix", System.Data.SqlDbType.VarChar, 50).Value = prefix + "%";
+            return db.CreateDataTable().ToList();
+        }
+
+        /// <summary>
+        /// Load single record to load
+        /// </summary>
+        /// <param name="db">Database connection</param>
+        /// <param name="empId">Employee to load</param>
+        /// <returns>Attributes for employee loaded</returns>
+        public Dictionary<string, string> LoadByEmpId(DBConnection db, int empId)
+        {
+            db.SQLCommand.CommandText =
+                @"SELECT [empID], [firstName], [lastName], [Active]
+                  FROM [OHEM]
+                  WHERE [empID] = @empId";
+            db.SQLCommand.Parameters.Add("@empId", System.Data.SqlDbType.Int).Value = empId;
+            return db.CreateDataTable().ToList().First();
+        }
+
+
+        /// <summary>
+        /// Create a new business partner
+        /// </summary>
+        /// <param name="sap">SAP connection</param>
+        /// <param name="firstName">First name</param>
+        /// <param name="lastName">Last name</param>
+        /// <param name="active">Employee activation</param>
+        /// <returns>Retrieve new object key</returns>
+        public int Create(
+            SAPConnection sap,
+            string firstName,
+            string lastName,
+            bool active)
+        {
+            IEmployeesInfo partner = sap.Company.GetBusinessObject(BoObjectTypes.oEmployeesInfo);
+            partner.FirstName = firstName;
+            partner.LastName = lastName;
+            partner.Active = active ? BoYesNoEnum.tYES : BoYesNoEnum.tNO;
+
+            sap.CheckResponse(partner.Add());
+            return int.Parse(sap.Company.GetNewObjectKey());
+        }
+
+        /// <summary>
+        /// Delete SAP record if exist
+        /// </summary>
+        /// <param name="sap">SAP connection</param>
+        /// <param name="key">Key to delete</param>
+        /// <returns></returns>
+        public bool Delete(SAPConnection sap, int key)
+        {
+            IEmployeesInfo partner = sap.Company.GetBusinessObject(BoObjectTypes.oEmployeesInfo);
+            if (partner.GetByKey(key))
+            {
+                sap.CheckResponse(partner.Remove());
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
---
+++ Test/HandlerTests/EmployeeLoaderTest.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SAPDIExamples;
+using SAPDIExamples.Handlers;
+using System.Collections.Generic;
+
+namespace Test.HandlerTests
+{
+    /// <summary>
+    /// Used to test data loaded for employee
+    /// </summary>
+    [TestClass]
+    public class EmployeeLoaderTest : BaseTest
+    {
+
+        /// <summary>
+        /// Handler to use in tests
+        /// </summary>
+        protected Employees Handler { get; set; }
+
+        /// <summary>
+        /// Initialize instance each test
+        /// </summary>
+        [TestInitialize]
+        public void Init() {
+            Handler = new Employees();
+        }
+
+        /// <summary>
+        /// Test employee creation
+        /// </summary>
+        [TestMethod]
+        public void ShouldCreateBusinessPartner()
+        {
+            using (SAPConnection sap = new SAPConnection())
+            using (DBConnection db = new DBConnection())
+            {
+                List<Dictionary<string, string>> existing =  Handler.LoadByPrefix(db, "SF-");
+                Assert.IsNotNull(existing);
+                foreach (Dictionary<string, string> i in existing)
+                {
+                    Assert.IsTrue(i.ContainsKey("empID"));
+                    int existingKey = int.Parse(i["empID"]);
+                    Assert.IsTrue(existingKey > 0);
+                    Handler.Delete(sap, existingKey);
+                }
+                existing = Handler.LoadByPrefix(db, "SF-");
+                Assert.IsNotNull(existing);
+                Assert.AreEqual(0, existing.Count);
+
+                int key1 = Handler.Create(sap, "SF-John", "Smith", true);
+                Assert.IsTrue(key1 > 0);
+
+                int key2 = Handler.Create(sap, "SF-Tom", "Lance", true);
+                Assert.IsTrue(key2 > 0);
+
+                Dictionary<string, string> john = Handler.LoadByEmpId(db, key1);
+                AssertEmployee(john, "SF-John", "Smith", true);
+
+                Dictionary<string, string> tom = Handler.LoadByEmpId(db, key2);
+                AssertEmployee(tom, "SF-Tom", "Lance", true);
+
+                existing = Handler.LoadByPrefix(db, "SF-");
+                Assert.IsNotNull(existing);
+                Assert.AreEqual(2, existing.Count);
+
+                Assert.IsTrue(Handler.Delete(sap, key1));
+                Assert.IsTrue(Handler.Delete(sap, key2));
+            }
+        }
+
+
+        /// <summary>
+        /// Test business partner data
+        /// </summary>
+        /// <param name="record">Record to test</param>
+        /// <param name="firstName">First name to test</param>
+        /// <param name="lastName">Last name to test</param>
+        /// <param name="active">Check if actuve</param>
+        private void AssertEmployee(
+            Dictionary<string, string> record,
+            string firstName,
+            string lastName,
+            bool active)
+        {
+            Assert.IsNotNull(record);
+            Assert.IsTrue(record.ContainsKey("firstName"));
+            Assert.IsTrue(record.ContainsKey("lastName"));
+            Assert.IsTrue(record.ContainsKey("Active"));
+
+            Assert.AreEqual(firstName, record["firstName"]);
+            Assert.AreEqual(lastName, record["lastName"]);
+            Assert.AreEqual(active ? "Y" : "F", record["Active"]);
+        }
+
+    }
+
+}
